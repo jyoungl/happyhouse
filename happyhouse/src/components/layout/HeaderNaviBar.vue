bb66d7de1291b3492d786f8f3e22689bf6416482
jyoungl <pmis118@naver.com>
2022-05-19 14:42:33 +0900

    folder update

---
+++ happyhouse/src/components/layout/HeaderNaviBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">
+        <router-link to="/">
+          <img
+            src="@/assets/ssafy_logo.png"
+            class="d-inline-block align-middle"
+            width="100px"
+            alt="ssafy"
+          />
+        </router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'board' }" class="link"
+              ><b-icon icon="journal" font-scale="1.5"></b-icon>
+              Q&A</router-link
+            ></b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNaviBar",
+};
+</script>
+
+<style></style>
